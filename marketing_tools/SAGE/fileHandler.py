f8d192e8f5dd1896f36798a5ddfe9bc098e69472
spectorco-stevent <stevent@spectorandco.com>
2024-12-10 10:37:38 -0500

    add more function to handle this change

--- marketing_tools/SAGE/fileHandler.py
+++ marketing_tools/SAGE/fileHandler.py
@@ -7,7 +7,10 @@ class classFileHandler:
         print("Test function")
         
     def test_item_code_change():
-        print("Item code change.")    
+        print("Item code change.") 
+        
+    def update_item_to_mongoDB():
+        print("Update item code to mongoDB")       
             
     def getLaunchItemList(self):
             #print(abc)
